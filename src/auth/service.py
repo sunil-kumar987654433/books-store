61150aa5274b72b97efba612b391701f7931fe60
sunil kumar <sunilkumar987654433@gmail.com>
2026-04-17 08:59:51 +0530

    generate new access token and auyhorization at api

--- src/auth/service.py
+++ src/auth/service.py
@@ -1,3 +1,5 @@
+import uuid
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.auth.models import User
 from src.auth.schema import UserCreate, UserLogin
@@ -7,9 +9,13 @@ from src.auth.utils import UserPassword, HashingToken
 from datetime  import datetime, timedelta, timezone
 from zoneinfo import ZoneInfo
 class UserService:
-    async def get_a_user_by_email(self, session: AsyncSession, email: str | None= None):
+    async def get_a_user_by_email(self, session: AsyncSession, email: str | None= None, user_uid: uuid.UUID|None = None):
         statement = select(User).where(
-                User.email == email
+            or_(
+                User.email == email,
+                User.uid == user_uid
+            )
+                
             
         )
         user = await session.execute(statement)
@@ -17,7 +23,7 @@ class UserService:
         
 
     async def create_user(self, user_data: UserCreate, session: AsyncSession):
-        is_user = await self.get_a_user_by_email(session, user_data.email)
+        is_user = await self.get_a_user_by_email(session = session, email = user_data.email)
         if is_user:
             raise HTTPException(
                 detail="Username or email already exist",
@@ -32,8 +38,36 @@ class UserService:
         await session.refresh(user)
         return user
     
+    async def create_new_access_token(self, user_uid: uuid.UUID, session: AsyncSession):
+        user = await self.get_a_user_by_email(session=session, user_uid = user_uid)
+        if user.is_active is False:
+            raise HTTPException(
+                detail='Inactive user',
+                status_code=status.HTTP_403_FORBIDDEN
+            )
+        access_token_expired_time = datetime.now(timezone.utc) + timedelta(minutes=5)
+        access_token= HashingToken().encode_data(
+                {
+                    "sub":str(user.uid),
+                    "exp": access_token_expired_time,
+                    "refresh": False
+                }
+            )
+            
+        if access_token_expired_time.tzinfo is None:
+            access_token_expired_time = access_token_expired_time.replace(tzinfo=timezone.utc)
+        access_token_expired_time = access_token_expired_time.astimezone(ZoneInfo("Asia/Kolkata")).isoformat()
+        return {
+                "access_token": access_token,
+                "access_token_expired_time": access_token_expired_time,
+         
+                "user_uid": str(user.uid),
+                "user_email": user.email
+            }
+        
+    
     async def login_user(self, user_data: UserLogin, session: AsyncSession):
-        user = await self.get_a_user_by_email(session, user_data.email)
+        user = await self.get_a_user_by_email(session=session, email=user_data.email)
         if user is None:
             raise HTTPException(
                 detail="email not exxist",
@@ -45,13 +79,12 @@ class UserService:
                 status_code=status.HTTP_403_FORBIDDEN
             )
         if UserPassword().verify_password(user_data.password, user.password_hash):
-            access_token_expired_time = datetime.now(timezone.utc) + timedelta(minutes=1)
+            access_token_expired_time = datetime.now(timezone.utc) + timedelta(minutes=5)
             refresh_token_expired_time = datetime.now(timezone.utc) + timedelta(days=2)
             access_token= HashingToken().encode_data(
                 {
                     "sub":str(user.uid),
                     "exp": access_token_expired_time,
-                    # "type": 'access'
                     "refresh": False
                 }
             )
