2facedb8478a796109275b12caf60e72197f656e
sunil kumar <sunilkumar987654433@gmail.com>
2026-04-20 18:03:40 +0530

    update in middleware file

--- src/middleware.py
+++ src/middleware.py
@@ -24,7 +24,10 @@ def register_middleware(app: FastAPI):
     
     app.add_middleware(
         TrustedHostMiddleware,
-        allowed_hosts = ["localhost", "127.0.0.1", 'https://books-store-22p9.onrender.com']
+        allowed_hosts = [
+            "localhost", 
+            "127.0.0.1", 
+            "https://books-store-22p9.onrender.com"]
     )
     
 
