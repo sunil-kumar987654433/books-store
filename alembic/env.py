c7a2ddc761361ae9ea7a907cd12cef09f343bb59
sunil kumar <sunilkumar987654433@gmail.com>
2026-04-27 20:10:45 +0530

    update dartabase url pd

--- alembic/env.py
+++ alembic/env.py
@@ -12,7 +12,7 @@ from src.config import Config
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url', Config.DATABASE_URL)
+config.set_main_option('sqlalchemy.url', Config.DATABASE_URL.replace("%", "%%"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
